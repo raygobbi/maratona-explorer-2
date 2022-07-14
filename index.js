c193e3c3360028995bb44b3fc31660d0adc7b0c6
Ray Gobbi <86269533+raygobbi@users.noreply.github.com>
2022-07-14 20:19:14 -0300

    Update index.js
    Colocando os botões do html de mundança de tema em funcionamento

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 /*No futuro fazer a mudança de tema funcionar corretamente com o slide do tipo range, mudando entre essas body, body.dark-blue, body.pink, body.dark-pink */
+
+function myFunction() {
+  /*if (range === '1') {
+    var element = document.body;
+    element.classList.toggle("");
+  } if ( === '2'){
+    var element = document.body;
+    element.classList.toggle("dark-blue");
+  } if ( === '3'){
+    var element = document.body;
+    element.classList.toggle("pink");
+  } else {
+    var element = document.body;
+    element.classList.toggle("dark-pink");
+  }*/
+  var element = document.body;
+  element.classList.toggle("dark-blue");
+}
+
+function myFunction1() {
+  var element = document.body;
+  element.classList.toggle("pink");
+}
+
+function myFunction2() {
+  var element = document.body;
+  element.classList.toggle("dark-pink");
+}
