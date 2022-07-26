0e17ebd551772216114c73d245d15054aad4b79a
Ray Gobbi <86269533+raygobbi@users.noreply.github.com>
2022-07-26 18:00:38 -0300

    Add files via upload
    Retirei os botões de mudança de tema e adicionei algumas mudanças no slider range para mudança de tema, mas ainda está com alguns bugs.

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 /*No futuro fazer a mudança de tema funcionar corretamente com o slide do tipo range, mudando entre essas body, body.dark-blue, body.pink, body.dark-pink */
 
-function myFunction() {
-  /*if (range === '1') {
+function rangeSlide(value) {
+  /*var element = document.body;
+  element.classList.toggle("dark-blue");*/
+  document.getElementById('rangeValue').innerHTML = value;
+  if (value === '0') {
     var element = document.body;
-    element.classList.toggle("");
-  } if ( === '2'){
+    element.classList.replace("dark-blue", "blue");
+  } if (value === '1'){
     var element = document.body;
-    element.classList.toggle("dark-blue");
-  } if ( === '3'){
+    element.classList.replace("blue","dark-blue");
+  } if (value === '2'){
     var element = document.body;
-    element.classList.toggle("pink");
-  } else {
+    element.classList.replace("dark-blue", "pink");
+  } if (value === '3'){
     var element = document.body;
-    element.classList.toggle("dark-pink");
-  }*/
-  var element = document.body;
-  element.classList.toggle("dark-blue");
+    element.classList.replace("pink", "dark-pink");
+  }  else {
+    var element = document.body;
+    element.classList.replace("dark-pink", "blue");
+  }
 }
 
-function myFunction1() {
-  var element = document.body;
-  element.classList.toggle("pink");
-}
-
-function myFunction2() {
-  var element = document.body;
-  element.classList.toggle("dark-pink");
-}
