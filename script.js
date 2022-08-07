4516b72c3b84aca1a7caf6bed724a90cc8e05d48
Ray Gobbi <86269533+raygobbi@users.noreply.github.com>
2022-08-07 19:45:34 -0300

    Update script.js
    Mudança no range, funcionando perfeitamente a mudança de tema agora

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /*No futuro fazer a mudança de tema funcionar corretamente com o slide do tipo range, mudando entre essas body, body.dark-blue, body.pink, body.dark-pink */
+/*Mudança no range, funcionando perfeitamente a mudança de tema agora*/
 
 function rangeSlide(value) {
   /*var element = document.body;
@@ -7,18 +8,24 @@ function rangeSlide(value) {
   if (value === '0') {
     var element = document.body;
     element.classList.replace("dark-blue", "blue");
+    element.classList.replace("dark-pink", "blue");
+    element.classList.replace("pink", "blue");
   } if (value === '1'){
     var element = document.body;
     element.classList.replace("blue","dark-blue");
+    element.classList.replace("pink","dark-blue");
+    element.classList.replace("dark-pink","dark-blue");
   } if (value === '2'){
     var element = document.body;
     element.classList.replace("dark-blue", "pink");
+    element.classList.replace("blue", "pink");
+    element.classList.replace("dark-pink", "pink");
   } if (value === '3'){
     var element = document.body;
     element.classList.replace("pink", "dark-pink");
-  }  else {
-    var element = document.body;
-    element.classList.replace("dark-pink", "blue");
-  }
+    element.classList.replace("blue", "dark-pink");
+    element.classList.replace("dark-blue", "dark-pink");
+  } 
 }
+
 
